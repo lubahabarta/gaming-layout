f1b73350bbfbae6b74486930dd6f0044abe73229
lubahabarta <lubahabarta@gmail.com>
2024-07-31 22:26:06 +0200

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "c.wallhere.com",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
